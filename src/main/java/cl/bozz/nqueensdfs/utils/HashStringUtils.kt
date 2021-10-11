c3e58036bf234c5c11662ddb4ab1e3545c901ffd
Eric Soppi <eric.soppi@gmail.com>
2021-10-11 09:49:40 -0700

    Use boolarray in rotations

--- src/main/java/cl/bozz/nqueensdfs/utils/HashStringUtils.kt
+++ src/main/java/cl/bozz/nqueensdfs/utils/HashStringUtils.kt
@@ -4,24 +4,20 @@ import cl.bozz.nqueensdfs.utils.RotationUtil.getAllRotations
 
 object HashStringUtils {
 
-    fun generateHashStringsFromMirrors(mirroredQueens: Set<Set<Int>>, n: Int): Set<String> {
+    fun generateHashStringsFromMirrors(mirroredQueens: Set<Set<Int>>, boardSize: Int): Set<String> {
         // This is slightly wasteful in that it doesn't check for repeats during generation. Worst-case scenario is a
         // board that's symmetrical on both axes, where rotations and mirroring do nothing... but that's still just 15
         // wasted board computations. Symmetry checks would be much more complicated and wasteful.
-        return mirroredQueens.map { mirroredQueen: Set<Int> -> getAllRotations(mirroredQueen, n) }
+        return mirroredQueens.map { mirroredQueen: Set<Int> -> getAllRotations(mirroredQueen, boardSize) }
                 .flatMap { it.toSet() }
-                .map { obj: Set<Int> -> generateHashString(obj, n) }.toSet()
+                .map { obj: BooleanArray -> generateHashString(obj) }.toSet()
     }
 
-    private fun generateHashString(queenList: Set<Int>, n: Int): String {
-        val boolArray = BooleanArray(n * n)
-        queenList.forEach{
-            boolArray[it] = true
-        }
+    private fun generateHashString(queenList: BooleanArray): String {
 
         var result = ""
-        for (i in boolArray.indices) {
-            if (boolArray[i]) {
+        for (i in queenList.indices) {
+            if (queenList[i]) {
                 result += i
                 result += "-"
             }
--- src/main/java/cl/bozz/nqueensdfs/utils/RotationUtil.kt
+++ src/main/java/cl/bozz/nqueensdfs/utils/RotationUtil.kt
@@ -1,7 +1,5 @@
 package cl.bozz.nqueensdfs.utils
 
-import java.util.stream.Collectors
-
 object RotationUtil {
 
     private val ROTATIONS = arrayOf(
@@ -10,35 +8,37 @@ object RotationUtil {
         Pair(Pair(0, 1), Pair(-1, 0))
     )
 
-    fun getAllRotations(queens: Set<Int>, n: Int): Set<Set<Int>> {
-        val results: MutableSet<Set<Int>> = HashSet()
-        results.add(queens)
+    fun getAllRotations(queens: Set<Int>, boardSize: Int): Set<BooleanArray> {
+        val results: MutableSet<BooleanArray> = HashSet()
+        val boolArray = BooleanArray(boardSize * boardSize)
+        queens.forEach{
+            boolArray[it] = true
+        }
+        results.add(boolArray)
         for (i in 0..2) {
-            val rotatedQueens1 = rotateNinetyDegrees(queens, i, n)
+            val rotatedQueens1 = rotateNinetyDegrees(queens, i, boardSize)
             results.add(rotatedQueens1)
         }
         return results
     }
 
-    private fun rotateNinetyDegrees(queens: Set<Int>, times: Int, n: Int): Set<Int> {
+    private fun rotateNinetyDegrees(queens: Set<Int>, times: Int, n: Int): BooleanArray {
         // Since the center of an even-sided board will be in the middle of a NQueensCell, we need to use decimals.
         // We use (n - 1) because cell positions start at 0 and end at (n - 1).
         val center = (n - 1).toDouble() / 2
 
         val (x, y) = ROTATIONS[times]
-        return queens.stream()
-                .map { cell: Int ->
-                    // New cell = (Old cell - center) * rotation + center.
-                    // Some basic geometry to get the rotation coefficients:
-                    // x_prime = x * cos(theta) - y * sin(theta)
-                    // y_prime = x * sin(theta) + y * cos(theta)
-                    val cellX = cell / n
-                    val cellY = cell % n
-                    val baseX = cellX.toDouble() - center
-                    val baseY = cellY.toDouble() - center
-                    val rotatedX = baseX * x.first + baseY * y.first + center
-                    val rotatedY = baseX * x.second + baseY * y.second + center
-                    rotatedX.toInt() * n +  rotatedY.toInt()
-                }.collect(Collectors.toCollection { HashSet() })
+        val boolArray = BooleanArray(n * n)
+        queens.forEach{
+            val cellX = it / n
+            val cellY = it % n
+            val baseX = cellX.toDouble() - center
+            val baseY = cellY.toDouble() - center
+            val rotatedX = baseX * x.first + baseY * y.first + center
+            val rotatedY = baseX * x.second + baseY * y.second + center
+            val rotatedCell = rotatedX.toInt() * n +  rotatedY.toInt()
+            boolArray[rotatedCell] = true
+        }
+        return boolArray
     }
 }
